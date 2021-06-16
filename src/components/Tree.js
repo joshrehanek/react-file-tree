66d2c3063ed057879ba011056a1e749a2482bb7f
Josh Rehanek <joshrehanek@gmail.com>
2021-06-15 18:40:24 -0700

    add map over rootNodes render TreeNodes; formats

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,49 +1,50 @@
 import React, { Component } from 'react';
 import values from 'lodash/values';
+import TreeNode from './TreeNode';
 
 
 const data = {
     '/root': {
-      path: '/root',
-      type: 'folder',
-      isRoot: true,
-      children: ['/root/josh', '/root/jsrehanek'],
+        path: '/root',
+        type: 'folder',
+        isRoot: true,
+        children: ['/root/josh', '/root/jsrehanek'],
     },
     '/root/josh': {
-      path: '/root/josh',
-      type: 'folder',
-      children: ['/root/josh/readme.md'],
+        path: '/root/josh',
+        type: 'folder',
+        children: ['/root/josh/readme.md'],
     },
     '/root/josh/readme.md': {
-      path: '/root/josh/readme.md',
-      type: 'file',
-      content: 'File Content goes here.'
+        path: '/root/josh/readme.md',
+        type: 'file',
+        content: 'File Content goes here.'
     },
     '/root/jsrehanek': {
-      path: '/root/jsrehanek',
-      type: 'folder',
-      children: ['/root/jsrehanek/projects', '/root/jsrehanek/vblogs'],
+        path: '/root/jsrehanek',
+        type: 'folder',
+        children: ['/root/jsrehanek/projects', '/root/jsrehanek/vblogs'],
     },
     '/root/jsrehanek/projects': {
-      path: '/root/jsrehanek/projects',
-      type: 'folder',
-      children: ['/root/jsrehanek/projects/treeview'],
+        path: '/root/jsrehanek/projects',
+        type: 'folder',
+        children: ['/root/jsrehanek/projects/treeview'],
     },
     '/root/jsrehanek/projects/treeview': {
-      path: '/root/jsrehanek/projects/treeview',
-      type: 'folder',
-      children: [],
+        path: '/root/jsrehanek/projects/treeview',
+        type: 'folder',
+        children: [],
     },
     '/root/jsrehanek/vblogs': {
-      path: '/root/jsrehanek/vblogs',
-      type: 'folder',
-      children: [],
+        path: '/root/jsrehanek/vblogs',
+        type: 'folder',
+        children: [],
     },
-  };
-  
+};
+
 
 export default class Tree extends Component {
-  
+
     state = {
         nodes: data,
     };
@@ -59,4 +60,17 @@ export default class Tree extends Component {
         return node.children.map(path => nodes[path]);
     }
 
+    render() {
+        const rootNodes = this.getRootNodes();
+        return (
+            <>
+                { rootNodes.map(node => (
+                    <TreeNode
+                        node={node}
+                        getChildNodes={this.getChildNodes}
+                    />
+                ))}
+            </>
+        )
+    }
 }
