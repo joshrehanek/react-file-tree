3e99aab9cd19e55a745810d41a9e43b72f333a83
Josh Rehanek <joshrehanek@gmail.com>
2021-06-15 18:48:39 -0700

    adds TreeNode imports

--- src/components/TreeNode.js
+++ src/components/TreeNode.js
@@ -1,4 +1,8 @@
-import React from 'react'
+import React from 'react';
+import { FaFile, FaFolder, FaFolderOpen, FaChevronDown, FaChevronRight } from 'react-icons/fa';
+import styled from 'styled-components';
+import last from 'lodash/last';
+import PropTypes from 'prop-types';
 
 export default function TreeNode() {
     return (
