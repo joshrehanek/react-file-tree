cb475d5718e8f231038fb449173c3f1b155942cb
Josh Rehanek <joshrehanek@gmail.com>
2021-06-16 19:42:56 -0700

    initial commit; adds imports

---
+++ src/components/FileExplorer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import styled from 'styled-components';
+import Tree from './Tree';
+
+export default class FileExplorer extends Component {
+    return (
+        <div>
+            
+        </div>
+    )
+}
