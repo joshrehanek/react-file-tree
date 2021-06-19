e9b35e7194c6f75aab7b6c1ff082c99de2db0d94
Josh Rehanek <joshrehanek@gmail.com>
2021-06-18 23:27:21 -0700

    adds state value

--- src/components/FileExplorer.js
+++ src/components/FileExplorer.js
@@ -3,9 +3,7 @@ import styled from 'styled-components';
 import Tree from './Tree';
 
 export default class FileExplorer extends Component {
-    return (
-        <div>
-            
-        </div>
-    )
+state = {
+    selectedFile: null;
+};
 }
